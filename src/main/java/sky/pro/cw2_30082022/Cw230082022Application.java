779d5d03b028b4241c5df625f6bf5d00ac0a04a0
ElenaR <llinusik@mail.ru>
2022-08-30 17:01:26 +0300

    Initial commit

---
+++ src/main/java/sky/pro/cw2_30082022/Cw230082022Application.java
@@ -0,0 +1,13 @@
+package sky.pro.cw2_30082022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cw230082022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cw230082022Application.class, args);
+    }
+
+}
---
+++ src/test/java/sky/pro/cw2_30082022/Cw230082022ApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.cw2_30082022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cw230082022ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
